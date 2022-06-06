6c318193deab5a49fd74eaa16fd0b035cc322371
Pierre-Yves Gillier <github@pygillier.me>
2022-06-06 22:03:45 +0200

    Task: init model

--- organize/settings.py
+++ organize/settings.py
@@ -46,7 +46,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'users'
+    'users',
+    'tasks'
 ]
 
 MIDDLEWARE = [
--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Task(models.Model):
+    title = models.CharField(max_length=200, blank=False)
+    description = models.TextField(blank=True)
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
